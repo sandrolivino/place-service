a8372de009015816f0c61e2551399cd4a72fd3bc
Sandro Livino <sandro.livino@gmail.com>
2023-06-18 15:08:56 -0300

    feat: generate slug automatically

--- src/main/java/br/com/sandrolivino/placeservice/domain/PlaceService.java
+++ src/main/java/br/com/sandrolivino/placeservice/domain/PlaceService.java
@@ -1,20 +1,23 @@
 package br.com.sandrolivino.placeservice.domain;
 
 import br.com.sandrolivino.placeservice.api.PlaceRequest;
+import com.github.slugify.Slugify;
 import reactor.core.publisher.Mono;
 
 public class PlaceService {
     private PlaceRepository placeRepository;
+    private Slugify slg;
 
     public PlaceService(PlaceRepository placeRepository) {
         this.placeRepository = placeRepository;
+        this.slg = Slugify.builder().build();
     }
 
     public Mono<Place> create(PlaceRequest placeRequest) {
         var place = new Place(
                 null,
                 placeRequest.name(),
-                placeRequest.slug(),
+                slg.slugify(placeRequest.name()),
                 placeRequest.state(),
                 placeRequest.createdAt(),
                 placeRequest.updatedAt());
